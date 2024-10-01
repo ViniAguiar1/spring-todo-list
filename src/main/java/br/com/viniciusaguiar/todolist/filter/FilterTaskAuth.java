4053cd923ca320171b01db6dce623a378b59193e
ViniAguiar1 <vinicius.aguiar1@icloud.com>
2024-09-30 21:37:38 -0300

    feat: adicionar validação de autenticação do usuário para criação de tarefas
    - Adicionada validação para garantir que apenas usuários autenticados possam criar tarefas.
    - Atualizado o TaskController para verificar a autenticação do usuário antes de permitir a criação de tarefas.
    - Melhorada a segurança do endpoint de criação de tarefas.

--- src/main/java/br/com/viniciusaguiar/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/viniciusaguiar/todolist/filter/FilterTaskAuth.java
@@ -1,22 +1,62 @@
 package br.com.viniciusaguiar.todolist.filter;
 
 import java.io.IOException;
+import java.util.Base64;
 
-import jakarta.servlet.Filter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.viniciusaguiar.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-public class FilterTaskAuth implements Filter {
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Autowired
+    private IUserRepository userRepository;
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        // Executar a lógica de autenticação
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
 
-        System.out.println("Filtro de autenticação");
-        chain.doFilter(request, response);
+        var servletPath = request.getServletPath();
+        if (servletPath.equals("/tasks/")) {
+            // Pegar a autenticação (user and password)
+            var authorization = request.getHeader("Authorization");
 
+            var authEncoded = authorization.substring("Basic".length()).trim();
+
+            byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+
+            var authString = new String(authDecode);
+
+            // ["viniaguiar1", "12345"]
+            String[] credentials = authString.split(":");
+            String username = credentials[0];
+            String password = credentials[1];
+
+            // Validar usuario
+            var user = this.userRepository.findByUsername(username);
+            if (user == null) {
+                response.sendError(401);
+            } else {
+                // Validar senha
+                var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                if (passwordVerify.verified) {
+                    request.setAttribute("idUser", user.getId());
+                    filterChain.doFilter(request, response);
+                } else {
+                    response.sendError(401);
+                }
+            }
+        } else {
+            filterChain.doFilter(request, response);
+        }
     }
-    
+
 }
--- src/main/java/br/com/viniciusaguiar/todolist/task/TaskController.java
+++ src/main/java/br/com/viniciusaguiar/todolist/task/TaskController.java
@@ -1,11 +1,14 @@
 package br.com.viniciusaguiar.todolist.task;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;  // Importando o UUID
 
 @RestController
 @RequestMapping("/tasks")
@@ -15,13 +18,17 @@ public class TaskController {
     private ITaskRepository taskRepository;
     
     public String postMethodName(@RequestBody String entity) {
-        //TODO: process POST request
-        
         return entity;
     }
     
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel) {
+    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+        System.out.println("Chegou no controller " + request.getAttribute("idUser"));
+        var idUser = request.getAttribute("idUser");
+
+        // Convertendo idUser para UUID
+        taskModel.setIdUser((UUID) idUser);  
+
         var task = this.taskRepository.save(taskModel);
         return task;
     }
